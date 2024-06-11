deec63cebe392412d2399b9a3eaf107f2a1c2cb1
dngiang <dongocgiang.edu@gmail.com>
2024-06-11 23:36:12 +0700

    🔧 Config env variable

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
+const { env } = require("./config");
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("The server backend API for HIT Moments is running 🌱");
 });
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+app.listen(env.port, () => {
+  console.log(`Server listening on port ${env.port}`);
 });
