a2089a68579485ea576b08e4652fcfd17276eb43
dngiang <dongocgiang.edu@gmail.com>
2024-06-14 18:58:51 +0700

    🌱 Connect database mongo

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const env = {
   port: process.env.PORT || 3000,
+  mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/hit-moments',
 };
 
 module.exports = env;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { env } = require('./config');
 
@@ -8,6 +9,16 @@ app.get('/', (req, res) => {
   res.send('The server backend API for HIT Moments is running 🌱');
 });
 
-app.listen(env.port, () => {
-  console.log(`Server listening on port ${env.port}`);
-});
+mongoose
+  .connect(env.mongoURI)
+  .then(() => {
+    console.log('Connected to MongoDB successfully!');
+  })
+  .then(() => {
+    app.listen(env.port, () => {
+      console.log(`Server is running on port ${env.port}!`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
