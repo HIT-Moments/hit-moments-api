507e625329e5140ecd44e75d8aa9852d01ca0e62
Tung <ductungltt@gmail.com>
2024-06-16 10:59:14 +0700

    ♻️ Change module import and remove log

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,10 +2,7 @@ const httpStatus = require('http-status');
 
 const { i18n } = require('../config');
 const { User } = require('../models');
-const { catchAsync } = require('../utils');
-const ApiError = require('../utils/ApiError');
-
-console.log(ApiError);
+const { ApiError, catchAsync } = require('../utils');
 
 const createUser = catchAsync(async (req, res, next) => {
   const existingEmail = await User.findOne({ email: req.body.email });
