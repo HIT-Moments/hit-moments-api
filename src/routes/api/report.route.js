f7d23433f30a77a533fe749adac0ecabfdd46a05
Bekend10 <tiendungk18a1@gmail.com>
2024-06-18 10:44:09 +0700

    🎨 Format code

--- src/routes/api/report.route.js
+++ src/routes/api/report.route.js
@@ -6,9 +6,14 @@ const { reportController } = require('../../controllers');
 
 const userRoute = express.Router();
 
-userRoute.route('/').post(validate(reportValidation.createReport), reportController.createReport).get(reportController.getList);
+userRoute
+  .route('/')
+  .post(validate(reportValidation.createReport), reportController.createReport)
+  .get(reportController.getList);
 
-userRoute.route('/:reportId').get(validate(reportValidation.getDetail), reportController.getDetail);
-userRoute.route('/:reportId').post(validate(reportValidation.deleteReport), reportController.deleteReport);
+userRoute
+  .route('/:reportId')
+  .get(validate(reportValidation.getDetail), reportController.getDetail)
+  .post(validate(reportValidation.deleteReport), reportController.deleteReport);
 
 module.exports = userRoute;
--- src/validations/report.validation.js
+++ src/validations/report.validation.js
@@ -3,27 +3,27 @@ const joi = require('joi');
 const createReport = {
   body: joi.object({
     description: joi.string().required(),
-    postId: joi.string().required()
+    postId: joi.string().required(),
   }),
 };
 
 const getDetail = {
   params: joi.object({
-    reportId: joi.string().required()
+    reportId: joi.string().required(),
   }),
 };
 
 const getList = {
   query: joi.object({
-    postId: joi.string().required()
+    postId: joi.string().required(),
   }),
 };
 
 const deleteReport = {
-    params: joi.object({
-      reportId: joi.string().required()
-    }),
-}
+  params: joi.object({
+    reportId: joi.string().required(),
+  }),
+};
 
 module.exports = {
   createReport,
