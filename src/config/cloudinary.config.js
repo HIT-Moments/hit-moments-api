08edaf40ddf1cc1604d57af2f1261263880053b0
huy2004-newbie <hahuy1810@gmail.com>
2024-06-20 09:38:00 +0700

    🔧  update cloudinary config

--- src/config/cloudinary.config.js
+++ src/config/cloudinary.config.js
@@ -1,7 +1,7 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({ 
-  cloud_name: 'dzotsteiv', 
-  api_key: '952641981189691', 
-  api_secret: 'WNe7WBVvPwh3hYbgeyJs19_30RE' 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET 
 });
