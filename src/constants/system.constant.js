3808992db2ca1b188d1beb18ba3b25ddc471c101
huy2004-newbie <hahuy1810@gmail.com>
2024-06-21 21:21:26 +0700

    ⚡️ Update

--- src/constants/system.constant.js
+++ src/constants/system.constant.js
@@ -4,8 +4,14 @@ const LANGUAGE_DEFAULT = 'vi';
 
 const HEADER_NAME = 'accept-language';
 
+const MAX_FILE_SIZE = 5 * 1024 * 1024;
+
+const TYPES_IMAGE_ALLOWED = ['image/png', 'image/jpg', 'image/jpeg'];
+
 module.exports = {
   LOCALES,
   HEADER_NAME,
   LANGUAGE_DEFAULT,
+  MAX_FILE_SIZE,
+  TYPES_IMAGE_ALLOWED,
 };
