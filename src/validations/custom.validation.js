40f039de6f61c7b6832483831f3eb7dad9513a98
Bekend10 <tiendungk18a1@gmail.com>
2024-06-21 23:18:45 +0700

    🐛 Fix conflict

--- src/validations/custom.validation.js
+++ src/validations/custom.validation.js
@@ -2,7 +2,7 @@ const { i18n } = require('../config');
 
 const objectId = (value, helpers) => {
   if (!value.match(/^[0-9a-fA-F]{24}$/)) {
-    return helpers.message(i8n.translate('custom.regexObjectId'));
+    return helpers.message(i18n.translate('custom.regexObjectId'));
   }
   return value;
 };
