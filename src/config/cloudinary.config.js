08e9445896c9f1a2caf7c673fe2abcd19b507b05
huy2004-newbie <hahuy1810@gmail.com>
2024-06-21 20:27:43 +0700

    🩹 Used env from env.config

--- src/config/cloudinary.config.js
+++ src/config/cloudinary.config.js
@@ -1,7 +1,11 @@
 const cloudinary = require('cloudinary').v2;
 
+const env = require('./env.config');
+
+
+
 cloudinary.config({ 
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.API_KEY, 
-  api_secret: process.env.API_SECRET 
+  cloud_name: env.cloudName,
+  api_key: env.apiKey, 
+  api_secret: env.apiSecret 
 });
