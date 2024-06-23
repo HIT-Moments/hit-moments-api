1bd08510a6619c99aba0aeba3c40293cf4c18319
Tung <ductungltt@gmail.com>
2024-06-23 19:55:35 +0700

    ♻️ Add role admin account on start

--- src/utils/initAdmin.js
+++ src/utils/initAdmin.js
@@ -12,6 +12,7 @@ async function initAdmin() {
     if (admin) {
       admin.fullname = fullname;
       admin.password = password;
+      admin.role = USER_ROLE.ADMIN;
     } else {
       admin = new User({ fullname, email, password, role: USER_ROLE.ADMIN });
     }
