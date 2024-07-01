1f3713c71b7d9d0f5ec44f53d0bd3c9cb52cdf46
Bekend10 <tiendungk18a1@gmail.com>
2024-07-01 19:54:31 +0700

    🌱 Assign permissions to friends

--- src/routes/api/friend.route.js
+++ src/routes/api/friend.route.js
@@ -1,19 +1,30 @@
 const express = require('express');
 
-const {validate , auth} = require('../../middlewares');
+const {validate , auth , author} = require('../../middlewares');
 const {friendValidation} = require('../../validations');
 const {friendController} = require('../../controllers');
 
 const friendRoute = express.Router();
 
+friendRoute.use(auth);
+
 friendRoute
   .route('/')
-  .post(validate(friendValidation.createFriend), friendController.createFriend)
+  .post(validate(friendValidation.createFriend), friendController.createFriend);
+
+friendRoute
+  .route('/:friendId')
+  .delete(validate(friendController.deleteFriend) , friendController.deleteFriend);
+
+friendRoute.use(author(["admin"]));
+
+friendRoute
+  .route('/')
   .get(friendController.getList);
 
 friendRoute
   .route('/:friendId')
   .get(validate(friendValidation.getDetail), friendController.getDetail)
-  .delete(validate(friendController.deleteFriend) , friendController.deleteFriend);
+
 
 module.exports = friendRoute;
