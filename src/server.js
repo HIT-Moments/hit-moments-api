50ca4cf0c8ac2cf4d20f56cd95c2581a8b4c1418
Tung <ductungltt@gmail.com>
2024-07-03 20:22:50 +0700

    🚀 Add cors middleware

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const morgan = require('morgan');
 const express = require('express');
 const mongoose = require('mongoose');
@@ -11,6 +12,8 @@ const { errorConverter, errorHandler } = require('./middlewares');
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 const isProduction = env.nodeEnv === 'production';
 const isDevelopment = env.nodeEnv === 'development';
